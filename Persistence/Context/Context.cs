893d35e244777ee34dc984bdee5d4d579a79fea8
szymo <szymon.tomala2@gmail.com>
2024-09-11 00:01:46 +0200

    db context added

---
+++ Persistence/Context/Context.cs
@@ -0,0 +1,17 @@
+using Domain.Entities;
+using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context;
+
+public class Context : DbContext
+{
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employee { get; set; }
+    public DbSet<Order> Order { get; set; }
+    public DbSet<Product> Product { get; set; }
+    public DbSet<User> User { get; set; }
+    
+    public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+    public DbSet<ProductAmount> ProductAmounts { get; set; }
+}
