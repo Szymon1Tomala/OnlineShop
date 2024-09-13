bb45c8c1a1006890052bbb658d13960d34325fe7
szymo <szymon.tomala2@gmail.com>
2024-09-13 02:21:56 +0200

    table renames

---
+++ Persistence/Migrations/20240912230923_Change table names.cs
@@ -0,0 +1,304 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changetablenames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Departments_DepartmentId",
+                table: "Employee");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_PhoneNumbers_PhoneNumberId",
+                table: "Employee");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAmounts_Order_OrderId",
+                table: "ProductAmounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAmounts_Product_ProductId",
+                table: "ProductAmounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_PhoneNumbers_PhoneNumberId",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Employee",
+                newName: "Employees");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_PhoneNumberId",
+                table: "Users",
+                newName: "IX_Users_PhoneNumberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_UserId",
+                table: "Orders",
+                newName: "IX_Orders_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Employee_PhoneNumberId",
+                table: "Employees",
+                newName: "IX_Employees_PhoneNumberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Employee_DepartmentId",
+                table: "Employees",
+                newName: "IX_Employees_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_PhoneNumbers_PhoneNumberId",
+                table: "Employees",
+                column: "PhoneNumberId",
+                principalTable: "PhoneNumbers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAmounts_Orders_OrderId",
+                table: "ProductAmounts",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAmounts_Products_ProductId",
+                table: "ProductAmounts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_PhoneNumbers_PhoneNumberId",
+                table: "Users",
+                column: "PhoneNumberId",
+                principalTable: "PhoneNumbers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_PhoneNumbers_PhoneNumberId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAmounts_Orders_OrderId",
+                table: "ProductAmounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAmounts_Products_ProductId",
+                table: "ProductAmounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_PhoneNumbers_PhoneNumberId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Employees",
+                newName: "Employee");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_PhoneNumberId",
+                table: "User",
+                newName: "IX_User_PhoneNumberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserId",
+                table: "Order",
+                newName: "IX_Order_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Employees_PhoneNumberId",
+                table: "Employee",
+                newName: "IX_Employee_PhoneNumberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employee",
+                newName: "IX_Employee_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Departments_DepartmentId",
+                table: "Employee",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_PhoneNumbers_PhoneNumberId",
+                table: "Employee",
+                column: "PhoneNumberId",
+                principalTable: "PhoneNumbers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAmounts_Order_OrderId",
+                table: "ProductAmounts",
+                column: "OrderId",
+                principalTable: "Order",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAmounts_Product_ProductId",
+                table: "ProductAmounts",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_PhoneNumbers_PhoneNumberId",
+                table: "User",
+                column: "PhoneNumberId",
+                principalTable: "PhoneNumbers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
